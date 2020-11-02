4065068a430302ca1a801e7a0a4ded8dbc5145d2
Lygon Bowen-West <l.bowenwest@googlemail.com>
2020-11-02 18:03:58 +0000

    Make overriding minecart drops far less disruptive

--- src/main/java/tweakyllama/cartomation/mixin/AbstractMinecartEntityMixin.java
+++ src/main/java/tweakyllama/cartomation/mixin/AbstractMinecartEntityMixin.java
@@ -1,27 +1,29 @@
 package tweakyllama.cartomation.mixin;
 
 import net.minecraft.entity.item.minecart.AbstractMinecartEntity;
-import net.minecraft.util.DamageSource;
-import org.spongepowered.asm.mixin.*;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.common.extensions.IForgeEntityMinecart;
+import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 import tweakyllama.cartomation.cart.module.CartModule;
 
 @Mixin(AbstractMinecartEntity.class)
-public abstract class AbstractMinecartEntityMixin {
+public abstract class AbstractMinecartEntityMixin implements IForgeEntityMinecart {
 
     private static final String drop_injection_target = "Lnet/minecraft/entity/item/minecart/AbstractMinecartEntity;entityDropItem(Lnet/minecraft/item/ItemStack;)Lnet/minecraft/entity/item/ItemEntity;";
 
     /**
-     * Stop the abstract minecart from dropping anything if option set in config
-     * Concrete classes are responsible for dropping the correct item
+     * Overwrite the item stack that the minecart would drop
+     *
+     * @param stack ItemStack to be dropped
+     * @return
      */
-    @Inject(method = "killMinecart", at = @At(value = "INVOKE", target = drop_injection_target), cancellable = true)
-    public void killMinecartHook(DamageSource source, CallbackInfo ci) {
+    @ModifyArg(method = "killMinecart", at = @At(value = "INVOKE", target = drop_injection_target))
+    public ItemStack modifyDrop(ItemStack stack) {
         if (CartModule.overwriteDrops) {
-            ci.cancel();
+            return this.getCartItem();
         }
+        return stack;
     }
-
 }
